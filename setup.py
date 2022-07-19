3a48a8dcf53afc38165afd0b7d7a8530ea23de2b
Apurv Sibal <apurvsibal@gmail.com>
2022-07-19 09:35:20 +0530

    updating setup for readme

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.rst").read_text()
 
 setup(
     name='pybanking',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
     version='0.1',
     license='MIT',
     author="Shorthills Tech",
