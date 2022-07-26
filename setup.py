1b437f93187a23ada14a9def4f223a044d5ad784
Apurv Sibal <apurvsibal@gmail.com>
2022-07-26 15:49:31 +0530

    documentation

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description = "Machine learning module for banking",
     long_description=long_description,
     long_description_content_type='text/x-rst',
-    version='0.9.1',
+    version='0.9.2',
     license='MIT',
     author="Shorthills Tech",
     author_email='apurv@shorthillstech.com',
