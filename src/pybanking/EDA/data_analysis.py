0b4a4c559cffa8ec73adb79b978718efe01942a4
nikhil0nk <nikhilkharbanda98@gmail.com>
2022-07-27 14:08:52 +0530

    data_analysis

--- src/pybanking/EDA/data_analysis.py
+++ src/pybanking/EDA/data_analysis.py
@@ -6,15 +6,14 @@ class Analysis:
     def sweetviz_analysis(self, df):
         subprocess.run(['pip', 'install', 'sweetviz'])
         import sweetviz as sv
-        return sv.analyze(df).show_html(filepath="Sweetviz_Report.html", open_browser=False, layout='widescreen', scale=None)
+        return sv.analyze(df)
 
     def dataprep_analysis(self, df):
         subprocess.run(['pip', 'install', 'dataprep'])
         from dataprep.eda import create_report
-        report = create_report(df)
-        return report.save("DataPrep_Report.html")
+        return create_report(df)
     
     def pandas_analysis(self, df):
         subprocess.run(['pip', 'install', 'pandas-profiling'])
         from pandas_profiling import ProfileReport
-        return ProfileReport(df).to_file("Pandas_Profiling_Report.html")
+        return ProfileReport(df)
