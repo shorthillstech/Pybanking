5893eaca5091a5b72d027c91df0cf886cc48e478
Apurv Sibal <apurvsibal@gmail.com>
2022-07-27 13:12:25 +0530

    updating repo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # read the contents of your README file
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.rst").read_text()
+long_description = (this_directory / "READMELIB.rst").read_text()
 
 setup(
     name='pybanking',
