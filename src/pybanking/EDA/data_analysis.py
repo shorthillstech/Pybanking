a1a53e745f9376a6d2a9a297b520f16f70f61962
nikhil0nk <nikhilkharbanda98@gmail.com>
2022-07-27 12:54:06 +0530

    data_analysis updated

--- src/pybanking/EDA/data_analysis.py
+++ src/pybanking/EDA/data_analysis.py
@@ -1,3 +1,4 @@
+from fileinput import filename
 import subprocess
 
 class Analysis:
@@ -9,9 +10,9 @@ class Analysis:
     def dataprep_analysis(self, df):
         subprocess.run(['pip', 'install', 'dataprep'])
         from dataprep.eda import plot, create_report
-        return create_report(df).save(), plot(df).save()
+        return create_report(df).save(filename='Dataprep_report.html')
     
     def pandas_analysis(self, df):
         subprocess.run(['pip', 'install', 'pandas-profiling'])
         from pandas_profiling import ProfileReport
-        return ProfileReport(df).to_file()
+        return ProfileReport(df).to_file("Profiling_report.html")
