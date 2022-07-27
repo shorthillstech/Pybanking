7c14d10125c2ba634ab5966151f6b704b7a52ce1
Apurv Sibal <apurvsibal@gmail.com>
2022-07-27 18:18:43 +0530

    desc

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 # read the contents of your README file
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "READMELIB.rst").read_text()
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='pybanking',
     description = "Machine learning module for banking",
     long_description=long_description,
-    long_description_content_type='text/x-rst',
-    version='0.9.7',
+    long_description_content_type='text/markdown',
+    version='0.9.8',
     license='MIT',
     author="Shorthills Tech",
     author_email='apurv@shorthillstech.com',
