a77e8a346b4875a4d476374af547e5ef49adb98f
nikhil0nk <nikhilkharbanda98@gmail.com>
2022-07-28 17:05:12 +0530

    update

--- src/pybanking/EDA/data_analysis.py
+++ src/pybanking/EDA/data_analysis.py
@@ -8,12 +8,13 @@ class Analysis:
         import sweetviz as sv
         if df.shape[1]>50:
             return sv.analyze(df, pairwise_analysis='off')
-        else:
-           return sv.analyze(df) 
+        return sv.analyze(df) 
 
     def dataprep_analysis(self, df):
         subprocess.run(['pip', 'install', 'dataprep'])
         from dataprep.eda import create_report
+        if df.shape[1]>50:
+            return create_report(df, mode="basic")
         return create_report(df)
     
     def pandas_analysis(self, df):
